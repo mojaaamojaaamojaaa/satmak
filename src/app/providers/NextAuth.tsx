192d5610a076d906f817d041042efef10ba7a9e1
mojaaamojaaamojaaa <47681057+mojaaamojaaamojaaa@users.noreply.github.com>
2024-02-13 02:32:40 +0900

    delete unusing files

--- src/app/providers/NextAuth.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
-//会員制ページを作成する際に使用。
-const NextAuthProvider = ({ children }: { children: ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default NextAuthProvider;
